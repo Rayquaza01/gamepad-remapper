bac799e4d961701388103b5af7a4892f45330a87
Joe Jarvis <joe.jarvis109@gmail.com>
2019-04-11 13:30:25 -0400

    start ini parser

---
+++ extension/ini_parser.js
@@ -0,0 +1,8 @@
+let INI = {
+    "parse": (raw, comment="#") => {
+        let obj = {};
+        // regex : \[(.*)]\s*([^[]*)
+       let raw = raw.split("\n");
+    },
+    "stringify": (json) => {}
+}
