4da4523c468ca77bba682a4c8b9dc3710aef8a30
Joe Jarvis <joe.jarvis109@gmail.com>
2019-04-15 16:05:06 -0400

    Finish mapping

--- app/gpmap.py
+++ app/gpmap.py
@@ -4,11 +4,35 @@ import pyautogui
 import json
 import urllib.parse
 import sys
+import math
+
+
+dirs = {"neg": -1, "pos": 1}
+axes = {"x": 0, "y": 1}
 
 
 def main():
     # map = json.loads(urllib.parse.unquote_plus(sys.argv[1]))
-    map = {"0": [["left"], "repeat"], "1": [["right"], "repeat"]}
+    map = {
+        "buttons": {
+            "0": ["left", "repeat"],
+            "1": ["right", "repeat"]
+        },
+        "axes": {
+            "0": {
+                "neg": ["left", "single"],
+                "pos": ["right", "single"]
+            }
+        },
+        "hats": {
+            "0": {
+                "x": {
+                    "neg": ["left", "repeat"],
+                    "pos": ["right", "repeat"]
+                }
+            }
+        }
+    }
     pygame.init()
     pygame.mixer.quit()
     js = pygame.joystick.Joystick(0)
@@ -17,15 +41,41 @@ def main():
         # use events for single button presses
         for event in pygame.event.get():
             if event.type == pygame.JOYBUTTONDOWN:
-                b_id = str(event.button)
-                if b_id in map and map[b_id][1] == "single":
+                id = str(event.button)
+                if id in map["buttons"] and map["buttons"][id][1] == "single":
                     if js.get_button(event.button) == 1:
-                        pyautogui.typewrite(map[b_id][0])
+                        pyautogui.press(map["buttons"][id][0])
+            if event.type == pygame.JOYAXISMOTION:
+                id = str(event.axis)
+                if id in map["axes"]:
+                    for dir in map["axes"][id].keys():
+                        if map["axes"][id][dir][1] == "single":
+                            if math.floor(event.value) == dirs[dir] and abs(event.value) > .9:
+                                pyautogui.press(map["axes"][id][dir][0])
+            if event.type == pygame.JOYHATMOTION:
+                id = str(event.hat)
+                if id in map["hats"]:
+                    for axis in map["hats"][id]:
+                        for dir in map["hats"][id][axis]:
+                            if map["hats"][id][axis][dir][1] == "single":
+                                if event.value[axes[axis]] == dirs[dir]:
+                                    pyautogui.press(map["hats"][id][axis][dir][0])
         # use loop for repeating
-        for button in map.keys():
-            if map[button][1] == "repeat":
+        for button in map["buttons"].keys():
+            if map["buttons"][button][1] == "repeat":
                 if js.get_button(int(button)) == 1:
-                    pyautogui.typewrite(map[button][0])
+                    pyautogui.press(map["buttons"][button][0])
+        for axis in map["axes"].keys():
+            for dir in map["axes"][axis]:
+                if map["axes"][axis][dir][1] == "repeat":
+                    if round(js.get_axis(int(axis))) == dirs[dir]:
+                        pyautogui.press(map["axes"][axis][dir][0])
+        for hat in map["hats"].keys():
+            for axis in map["hats"][hat]:
+                for dir in map["hats"][hat][axis]:
+                    if map["hats"][hat][axis][dir][1] == "repeat":
+                        if js.get_hat(int(hat))[axes[axis]] == dirs[dir]:
+                            pyautogui.press(map["hats"][hat][axis][dir][0])
     pygame.time.Clock(60)
 
 
