bf626035b9305546ffd54636b9c9a77bdded7ce3
Joe Jarvis <joe.jarvis109@gmail.com>
2019-04-16 00:52:25 -0400

    Fix high cpu usage, single axis presses work

--- app/gpmap.py
+++ app/gpmap.py
@@ -11,17 +11,32 @@ dirs = {"neg": -1, "pos": 1}
 axes = {"x": 0, "y": 1}
 
 
+def posOrNeg(num):
+    if num > 0:
+        return 1
+    elif num < 0:
+        return -1
+    elif num == 0:
+        return 0
+
+
 def main():
     # map = json.loads(urllib.parse.unquote_plus(sys.argv[1]))
     map = {
         "buttons": {
-            "0": ["left", "repeat"],
-            "1": ["right", "repeat"]
+            "1": ["up", "single"],
+            "0": ["z", "single"],
+            "4": ["c", "single"],
+            "5": ["c", "single"]
         },
         "axes": {
             "0": {
-                "neg": ["left", "single"],
-                "pos": ["right", "single"]
+                "neg": ["left", "repeat"],
+                "pos": ["right", "repeat"]
+            },
+            "1": {
+                "neg": ["space", "single"],
+                "pos": ["down", "repeat"]
             }
         },
         "hats": {
@@ -29,12 +44,17 @@ def main():
                 "x": {
                     "neg": ["left", "repeat"],
                     "pos": ["right", "repeat"]
+                },
+                "y": {
+                    "pos": ["space", "single"],
+                    "neg": ["down", "repeat"]
                 }
             }
         }
     }
-    pygame.init()
-    pygame.mixer.quit()
+    pygame.joystick.init()
+    pygame.display.init()
+    clock = pygame.time.Clock()
     js = pygame.joystick.Joystick(0)
     js.init()
     while True:
@@ -44,14 +64,14 @@ def main():
                 id = str(event.button)
                 if id in map["buttons"] and map["buttons"][id][1] == "single":
                     if js.get_button(event.button) == 1:
-                        pyautogui.press(map["buttons"][id][0])
+                        pyautogui.typewrite([map["buttons"][id][0]])
             if event.type == pygame.JOYAXISMOTION:
                 id = str(event.axis)
                 if id in map["axes"]:
                     for dir in map["axes"][id].keys():
                         if map["axes"][id][dir][1] == "single":
-                            if math.floor(event.value) == dirs[dir] and abs(event.value) > .9:
-                                pyautogui.press(map["axes"][id][dir][0])
+                            if round(event.value) == dirs[dir] and abs(round(event.value) - event.value) <= .001:
+                                pyautogui.typewrite([map["axes"][id][dir][0]])
             if event.type == pygame.JOYHATMOTION:
                 id = str(event.hat)
                 if id in map["hats"]:
@@ -59,24 +79,24 @@ def main():
                         for dir in map["hats"][id][axis]:
                             if map["hats"][id][axis][dir][1] == "single":
                                 if event.value[axes[axis]] == dirs[dir]:
-                                    pyautogui.press(map["hats"][id][axis][dir][0])
+                                    pyautogui.typewrite([map["hats"][id][axis][dir][0]])
         # use loop for repeating
         for button in map["buttons"].keys():
             if map["buttons"][button][1] == "repeat":
                 if js.get_button(int(button)) == 1:
-                    pyautogui.press(map["buttons"][button][0])
+                    pyautogui.typewrite([map["buttons"][button][0]])
         for axis in map["axes"].keys():
             for dir in map["axes"][axis]:
                 if map["axes"][axis][dir][1] == "repeat":
                     if round(js.get_axis(int(axis))) == dirs[dir]:
-                        pyautogui.press(map["axes"][axis][dir][0])
+                        pyautogui.typewrite([map["axes"][axis][dir][0]])
         for hat in map["hats"].keys():
             for axis in map["hats"][hat]:
                 for dir in map["hats"][hat][axis]:
                     if map["hats"][hat][axis][dir][1] == "repeat":
                         if js.get_hat(int(hat))[axes[axis]] == dirs[dir]:
-                            pyautogui.press(map["hats"][hat][axis][dir][0])
-    pygame.time.Clock(60)
+                            pyautogui.typewrite([map["hats"][hat][axis][dir][0]])
+        clock.tick(60)
 
 
 if __name__ == "__main__":
