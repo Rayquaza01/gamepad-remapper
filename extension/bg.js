415e080bfe7475bf617f0b2fbd897a88dccf1dfd
Joe Jarvis <joe.jarvis109@gmail.com>
2019-04-20 00:42:10 -0400

    Edit options with yaml

--- extension/bg.js
+++ extension/bg.js
@@ -1,3 +1,25 @@
+/* globals jsyaml */
+function defaultValues(object, settings) {
+    for (let key in settings) {
+        if (!object.hasOwnProperty(key)) {
+            object[key] = settings[key];
+        }
+    }
+    return object;
+}
+
+async function setOpts() {
+    let sample = await (await fetch(
+        browser.extension.getURL("sample_config.yaml")
+    )).text();
+    let res = await browser.storage.local.get();
+    res = defaultValues(res, {
+        options_yaml: sample,
+        options_json: jsyaml.safeLoad(sample)
+    });
+    browser.storage.local.set(res);
+}
+
 function onMessage(message) {
     console.log(message);
 }
@@ -12,10 +34,14 @@ function sendMessage(message) {
         port.postMessage({
             action: "stop"
         });
-    } else if (message.action === "updateConsts") {
+    } else if (message.action === "restart") {
         port.postMessage({
-            action: "updateConsts",
+            action: "restart",
             config: message.config
+        });
+    } else if (message.action === "tester") {
+        port.postMessage({
+            action: "tester"
         });
     }
 }
@@ -23,3 +49,4 @@ function sendMessage(message) {
 const port = browser.runtime.connectNative("gamepadremapper.r01");
 port.onMessage.addListener(onMessage);
 browser.runtime.onMessage.addListener(sendMessage);
+browser.runtime.onInstalled.addListener(setOpts);
--- extension/options.js
+++ extension/options.js
@@ -1,35 +1,42 @@
-function zip(arr1, arr2) {
-	let val = {};
-	if (
-		arr1.length === arr2.length && // both same length
-	    arr1.every((item, index) => arr1.indexOf(item) === index) // keys contain only unique values
-	) {
-		for (let i = 0; i < arr1.length; i++) {
-			val[arr1[i]] = arr2[i];
-		}
-		return val;
-	} else {
-		return false;
-	}
+/* globals jsyaml */
+const options_yaml = document.getElementById("options_yaml");
+const options_json = document.getElementById("options_json");
+
+function launchTester() {
+    browser.runtime.sendMessage({ action: "tester" });
 }
 
-async function restoreSettings() {
-	let res = await browser.storage.local.get("options");
-	Object.keys(res.options).forEach(item => document.getElementById("opt-" + item).value = res[item])
+function tabSpace(e) {
+    // from https://stackoverflow.com/questions/6637341/use-tab-to-indent-in-textarea/14166052#14166052
+    if (e.key === "Tab") {
+        e.preventDefault();
+        let start = this.selectionStart;
+        this.value =
+            this.value.substring(0, start) +
+            "  " +
+            this.value.substring(this.selectionEnd);
+        this.selectionEnd = start + 2;
+    }
 }
 
-async function saveSettings() {
-	let res = await browser.storage.local.get("options");
-	let settings = Array.from(document.getElementsByClassName("opt"));
-	let keys = Object.keys(res.options);
-	let vals = settings.map(item => res.options[item.id.substring(4)]);
-	await browser.storage.local.set({ options: zip(keys, vals) });
+async function load() {
+    let res = await browser.storage.local.get("options_yaml");
+    options_yaml.value = res.options_yaml;
+    options_json.value = JSON.stringify(jsyaml.safeLoad(res.options_yaml), null, 4);
 }
 
-function main() {
-	restoreSettings();
-    // main
+async function save() {
+    let sample = await (await fetch(
+        browser.extension.getURL("sample_config.yaml")
+    )).text();
+    options_json.value = JSON.stringify(jsyaml.safeLoad(options_yaml.value), null, 4);
+    browser.storage.local.set({
+        options_yaml: options_yaml.value || sample,
+        options_json: JSON.parse(options_json.value) || jsyaml.safeLoad(sample)
+    });
 }
 
-document.getElementsById("settings").addEventListener("input", saveSettings);
-document.addEventListener("DOMContentLoaded", main);
+options_yaml.addEventListener("keydown", tabSpace);
+document.getElementById("save").addEventListener("click", save);
+document.getElementById("open").addEventListener("click", launchTester);
+document.addEventListener("DOMContentLoaded", load);
